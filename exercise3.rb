d513d750a7a95a7acaf8912c9e5c4d31b6d83b27
Joe Bianchi <joseph.bianchi3@gmail.com>
2017-08-08 14:47:16 -0400

    finished exercise 3

--- exercise3.rb
+++ exercise3.rb
@@ -1,1 +1,21 @@
 puts "what is your name?"
+
+
+user_name = gets.chomp
+
+puts "Hello, #{user_name}"
+
+# ask user how old they are
+
+puts "what year were you born?"
+user_age = gets.chomp
+
+
+puts "year of birth is #{user_age}"
+
+
+# if statements
+
+if my_number > 1
+  puts "the number is greater than 1"
+end
