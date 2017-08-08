3ad0bbd9bf5650d15329ae55a61dfc4038dfb639
Joe Bianchi <joseph.bianchi3@gmail.com>
2017-08-08 14:07:49 -0400

    exercise 2 fin

---
+++ exercise2.rb
@@ -0,0 +1,19 @@
+# tip for a $55 meal
+bill = 55
+tip_percentage = 0.15
+tip_amount = bill * tip_percentage
+puts tip_amount
+
+
+# adding string and an integer w/ '+' operator
+puts "twenty" + 22.to_s
+
+# string interpolation
+puts "I paid wayyy too much for that container - almost $#{45628 * 7839}"
+
+# value of the expression
+
+puts (10 < 20 && 30 < 20) || !(10 == 11)
+
+# ----------variable
+my_vairable = "my_variable now contains this string"
