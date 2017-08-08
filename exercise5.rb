a0f99c93cb05b8da03b156ab22e5ed45ec427e1e
Joe Bianchi <joseph.bianchi3@gmail.com>
2017-08-08 17:49:39 -0400

    6.2

--- exercise5.rb
+++ exercise5.rb
@@ -3,18 +3,38 @@
 puts "press any key"
 action_taken = gets.chomp
 distance_walked = 0
+energy = 20
 
 while action_taken != "go home"
+  if energy <= 0
+    puts "too tired"
+    break
+  end
   puts "action to take - walk or run?"
   action_taken = gets.chomp.to_s
   if action_taken == "walk"
     distance_walked += 1
-    puts "distance from home #{distance_walked}"
-
-  elsif action_taken == "run"
+    energy += 2
+    puts "distance from home #{distance_walked}, energy level: #{energy}"
+  elsif action_taken == "run" || energy > 0
     distance_walked += 5
-    puts "distance from home #{distance_walked}"
+    energy -= 5
+    puts "distance from home #{distance_walked}, energy level: #{energy}"
   elsif action_taken == "go home"
     puts "BYE BYE"
+  elsif action_taken == "eat"
+    energy += 5
+    puts "distance from home #{distance_walked}, energy level: #{energy}"
+  elsif action_taken == "sleep"
+    energy += 5
+    puts "distance from home #{distance_walked}, energy level: #{energy}"
+
+
+
+
+
+
+
+
 end
 end
